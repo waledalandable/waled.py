ceb1eb976f5caff2357c747302bb013fd1c29118
waledalandable <156304076+waledalandable@users.noreply.github.com>
2026-04-23 01:25:26 +0400

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
+from pyrogram import Client, idle
 import os
+
+print("Starting Bot...")
 
 API_ID = int(os.getenv("API_ID"))
 API_HASH = os.getenv("API_HASH")
 BOT_TOKEN = os.getenv("BOT_TOKEN")
+
 app = Client(
     "bot",
     api_id=API_ID,
@@ -13,7 +17,8 @@ app = Client(
 @app.on_message()
 async def start(_, msg):
     if msg.text == "/start":
-        await msg.reply("🎧 البوت يعمل بنجاح 👑")
-app.star()
+        await msg.reply("🎧 البوت يعمل 👑")
+
+app.start()
 print("Bot Running...")
 idle()
