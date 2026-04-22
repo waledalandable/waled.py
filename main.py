0c4cfd107c673c590d4a18efe8c91c2738c45128
waledalandable <156304076+waledalandable@users.noreply.github.com>
2026-04-23 01:02:16 +0400

    Add files via upload

---
+++ main.py
@@ -0,0 +1,19 @@
+frimport os
+
+API_ID = int(os.getenv("API_ID"))
+API_HASH = os.getenv("API_HASH")
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+app = Client(
+    "bot",
+    api_id=API_ID,
+    api_hash=API_HASH,
+    bot_token=BOT_TOKEN
+)
+
+@app.on_message()
+async def start(_, msg):
+    if msg.text == "/start":
+        await msg.reply("🎧 البوت يعمل بنجاح 👑")
+app.star()
+print("Bot Running...")
+idle()
